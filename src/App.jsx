5ec68609118eeeb7bbabcf390ee8f55d02ca7da1
Matias Figueroa <kevinmsotof@gmail.com>
2025-07-28 19:53:13 -0400

    landing-page v1 - Matias Figueroa

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
-import AboutSecion from "./components/AboutSection/AboutSecion";
-import Coaches from "./components/Coaches/Coaches";
-import HeroSection from "./components/HeroSection.jsx/HeroSection";
-import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Plans from "./components/Plans.jsx/plans";
-import ScheduleSection from "./components/ScheduleSection/ScheduleSection";
-import ContactSection from "./components/ContactSection/ContactSection";
-import FooterSection from "./components/FooterSection/FooterSection";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
