195268df132cee6688d40272baf4e8d1ef480c97
Matias Figueroa <kevinmsotof@gmail.com>
2025-07-29 00:56:18 -0400

    Animaciones Listas - Matias Figueroa

--- src/components/TestimonialsSection/TestimonialsSection.jsx
+++ src/components/TestimonialsSection/TestimonialsSection.jsx
@@ -13,7 +13,7 @@ const TestimonialsSection = ({ data }) => {
             {duplicatedData.map((item, idx) => (
               <div
                 key={idx}
-                className="bg-[#1a1a1a] p-6 rounded-xl shadow-md hover:scale-105 transition-transform duration-300"
+                className="bg-[#1a1a1a] p-6 rounded-xl w-100 shadow-md hover:scale-105 transition-transform duration-300"
               >
                 <h3 className="text-lg font-semibold text-orange-400">
                   {item.name}
