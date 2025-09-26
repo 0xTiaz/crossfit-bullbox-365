7701b7c0e55154aabc8e950e6edf685a759edbb3
Matias Figueroa <kevinmsotof@gmail.com>
2025-09-26 10:43:50 -0300

    Theme and Colors Changed - Matias Figueroa

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -13,16 +13,14 @@ const AboutSection = () => {
       ></div>
       <div className="absolute inset-0 bg-black/60 md:bg-black/70 z-10"></div>
       <div className="relative z-20 max-w-4xl mx-auto text-justify md:text-center">
-        <h2 className="text-3xl md:text-4xl font-bold mb-6 text-orange-500">
+        <h2 className="text-3xl md:text-4xl font-bold mb-6 text-primary">
           Nuestra Historia
         </h2>
         <p className="text-white md:text-xl leading-relaxed">
           En{" "}
-          <span className="font-bold text-orange-500">
-            Crossfit Bullbox 365
-          </span>
-          , no solo entrenamos cuerpos, entrenamos carácter. Nacimos con la
-          visión de crear una comunidad fuerte, inclusiva y motivada, donde cada
+          <span className="font-bold text-primary">Crossfit Bullbox 365</span>,
+          no solo entrenamos cuerpos, entrenamos carácter. Nacimos con la visión
+          de crear una comunidad fuerte, inclusiva y motivada, donde cada
           persona —desde principiantes hasta atletas— encuentre un espacio para
           superarse día a día. Inspirados por la raza <em>bull terrier</em>,
           somos decididos, resistentes y leales. Nuestra misión es acompañarte
